c1c9132124b9d88b15a7f5ae9fe7fbeeb534ef12
Jossy Solá <jossysola@gmail.com>
2024-08-22 00:03:48 -0600

    Styles and adds functionality to Back button

--- app/atomic/organisms/article.tsx
+++ app/atomic/organisms/article.tsx
@@ -1,11 +1,20 @@
-import { ArrowLeft } from "@geist-ui/icons"
+import { ArrowLeftCircle } from "@geist-ui/icons"
+import { useNavigate } from "@remix-run/react"
 
 export default function Article ({children}: {
     children: React.ReactNode
 }) {
+    const navigate = useNavigate();
+
     return (
         <>
-            <ArrowLeft />
+            <button style={{cursor: "pointer"}} onClick={(e) => {
+                    e.preventDefault();
+                    navigate(-1);
+                }} aria-label="Go Back button">
+                <ArrowLeftCircle />
+            </button>
+            
             <article className="GeistReg w-full text-left border-t-2 md:border-none">
                 {children}
             </article>
