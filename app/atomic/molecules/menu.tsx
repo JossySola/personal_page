4b635e69039015bd09087f070c9e9ba0217995fd
Jossy Solá <jossysola@gmail.com>
2024-08-24 01:41:34 -0600

    Modifies rotation and translation on Bubble components

--- app/atomic/molecules/menu.tsx
+++ app/atomic/molecules/menu.tsx
@@ -13,10 +13,10 @@ export default function Menu () {
     return (
         <nav className="flex flex-col">
             <Social />
-            <Bubble text={nav[lang][1]} style="rotate-2" url="articles"/>
-            <Bubble text={nav[lang][2]} style="rotate-3" url="art"/>
-            <Bubble text={nav[lang][3]} style="rotate-[-3deg]" url="dev"/>
-            <Bubble text={nav[lang][4]} style="rotate-6 translate-y-1" url="contact"/>
+            <Bubble text={nav[lang][1]} style="rotate-[-6deg] translate-y-2" url="articles"/>
+            <Bubble text={nav[lang][2]} style="rotate-2 translate-y-1" url="art"/>
+            <Bubble text={nav[lang][3]} style="rotate-2 translate-y-0.5" url="dev"/>
+            <Bubble text={nav[lang][4]} url="contact"/>
         </nav>
     )
 }
