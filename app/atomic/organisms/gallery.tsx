848b5bce0bc173e2262f764f8a494e64250eaebe
Jossy Solá <jossysola@gmail.com>
2024-08-29 11:23:20 -0600

    Removes conditional rendering

--- app/atomic/organisms/gallery.tsx
+++ app/atomic/organisms/gallery.tsx
@@ -1,30 +1,18 @@
 import { usePictures } from "~/hooks/custom";
 import Bullets from "../atoms/bullets";
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 export default function Gallery () {
     const [index, setIndex] = useState<number>(0);
     const [prev, setPrev] = useState(null);
     const [current, setCurrent] = useState();
     const [next, setNext] = useState();
-    const ref = useRef(0);
-    ref.current = window.screen.width;
 
     const pictures = usePictures("ceramics");
-    
-    
-    if (typeof ref.current === "undefined" || ref.current < 768) {
-        return (
-            <>
-                <Bullets quantity={pictures.length} index={index}/>
-                <p>undefined</p>
-            </>
-        )
-    } else {
-        return (
-            <>
-                <Bullets quantity={pictures.length} index={index}/>
-            </>
-        )
-    }
+  
+    return (
+        <>
+            <Bullets quantity={pictures.length} index={index}/>
+        </>
+    )
 }
