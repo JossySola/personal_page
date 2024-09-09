c417b333f7c7c64af8a57261af74c3f6bab21af7
Jossy Solá <jossysola@gmail.com>
2024-09-08 18:57:19 -0600

    Changes width in MD breakpoint

--- app/atomic/organisms/category.tsx
+++ app/atomic/organisms/category.tsx
@@ -8,7 +8,7 @@ export default function Category ({children}: {
 
     return (
         <> 
-            <article className="GeistReg flex items-center justify-center mt-6 w-full text-left border-t-2 md:border-none  md:min-w-[450px] md:max-w-[700px] md:h-screen md:overflow-y-auto">
+            <article className="GeistReg flex items-center justify-center mt-6 w-full text-left border-t-2 md:border-none md:w-fit md:max-w-[700px] md:h-screen md:overflow-y-auto">
                 <div className="p-6 flex flex-col items-center md:max-h-screen md:overflow-y-auto items-center scrollbar-thin scrollbar-thumb-gray-900">
                     <button className="cursor-pointer m-[20px]" onClick={(e) => {
                             e.preventDefault();
