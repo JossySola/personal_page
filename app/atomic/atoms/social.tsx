95d47662fc5d85a0ffde65e9b371f1c834757788
Jossy Solá <jossysola@gmail.com>
2024-09-08 22:12:59 -0600

    Changes instagram link

--- app/atomic/atoms/social.tsx
+++ app/atomic/atoms/social.tsx
@@ -2,7 +2,7 @@ export default function Social () {
     return (
         <ul className="inline-flex items-center content-center justify-center m-6">
             <li style={{margin: 10}}><a target="_blank" href="https://github.com/JossySola"><img width={32} height={32} style={{width: 32, height: "auto"}} src="/github.webp" alt="Github" title="GitHub"/></a></li>
-            <li style={{margin: 10}}><a target="_blank" href="https://www.instagram.com/jossysola/"><img width={32} height={32} style={{width: 32, height: "auto"}} src="/Instagram_Glyph_Gradient.webp" alt="Instagram" title="Instagram"/></a></li>
+            <li style={{margin: 10}}><a target="_blank" href="https://www.instagram.com/jossysolart/"><img width={32} height={32} style={{width: 32, height: "auto"}} src="/Instagram_Glyph_Gradient.webp" alt="Instagram" title="Instagram"/></a></li>
             <li style={{margin: 10}}><a target="_blank" href="https://www.linkedin.com/in/jossysola/"><img width={32} height={32} style={{width: 32, height: "auto"}} src="/linkedin.webp" alt="LinkedIn" title="LinkedIn"/></a></li>
         </ul>
     )
