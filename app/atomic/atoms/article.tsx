e7e47106dcea6327ecbb6bb3fa38340ce99b3901
Jossy Solá <jossysola@gmail.com>
2024-09-08 19:03:26 -0600

    Adds hover font color change to <summary>

--- app/atomic/atoms/article.tsx
+++ app/atomic/atoms/article.tsx
@@ -6,7 +6,7 @@ export default function Article ({name, document}: {
 }) {
     return (
         <details className="mb-6 w-full p-6 rounded-lg border shadow-md open:bg-[#314D68] open:text-white transition-all">
-            <summary className="font-[Geist] text-center text-xl cursor-pointer list-none rounded-lg">{name}</summary>
+            <summary className="font-[Geist] text-center text-xl cursor-pointer list-none rounded-lg md:hover:text-[#4594DF]">{name}</summary>
             <div className="mt-6 mb-3 markdown">{document}</div>
         </details>
     )
